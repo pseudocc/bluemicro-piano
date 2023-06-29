1bd348b2c99def67f782fc244468015166e4a7b3
pseudoc <atlas.yu@canonical.com>
2023-06-29 13:37:35 +0800

    change the GPIO pins

--- piano/hardware/nice_nano_v2/hardware_config.h
+++ piano/hardware/nice_nano_v2/hardware_config.h
@@ -7,7 +7,7 @@
 #define MATRIX_ROWS 1
 #define MATRIX_COLS 9
 #define MATRIX_ROW_PINS { 33 }
-#define MATRIX_COL_PINS { 8, 14, 5, 10, 9, 15, 4, 0, 1 }
+#define MATRIX_COL_PINS { 10, 3, 13, 43, 38, 45, 17, 8, 6 }
 #define UNUSED_PINS {}
 
 // used in debug_cli.cpp to bypass 0.14 and 0.16 that are directly connected to 0.18 (reset)
@@ -16,9 +16,9 @@
 #define DIODE_DIRECTION COL2ROW
 #define BACKLIGHT_PWM_ON 0
 #define BATTERY_TYPE BATT_LIPO
-#define VBAT_PIN  4
-#define VCC_PIN 13
+#define VBAT_PIN  31
+#define VCC_PIN 12
 #define VCC_POLARITY_ON 0
-#define  STATUS_BLE_LED_PIN  15  //blue = 0.15
+#define STATUS_BLE_LED_PIN  15  //blue = 0.15
 
 #endif
